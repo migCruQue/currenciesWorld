ee3a65cdbd76c1a8881533946e6d55bc65b409b2
Miguel Cruz Quesada <miguel.cruz.quesada@gmail.com>
2022-02-01 18:02:02 +0000

    playing with data to provide a table

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -1,28 +1,78 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 import "../../styles/Home/home.scss";
+import { supportedCurrencies, latestRates } from "../../config/dataTesting";
+
+
+// ARRAY OF OBJECTS WITH TRADITIONAL CURRENCIES currency.countryName !==  "Global"
+let traditionalCurrencies = supportedCurrencies.filter(currency => currency.countryName !==  "Global");
+// ARRAY OF OBJECTS WITH CRYPTO CURRENCIES currency.countryCode ===  "Crypto"
+let cryptoCurrencies = supportedCurrencies.filter(currency => currency.countryCode ===  "Crypto");
+// ARRAY OF OBJECTS WITH METAL CURRENCIES currency.countryCode ===  "Metal", I'm not planning to use it as the free subscription doesnt
+// provide much information.
+// let metalCurrencies = supportedCurrencies.filter(currency => currency.countryCode ===  "Metal");
+
+//OBJECT WITH COUNTRY CODE KEY AND RATE VALUE {{"countrycode": "rate"}, {}..}
+let rates = latestRates.rates;
+//ARRAY WITH ALL THE KEYS CONTAINS IN THE RATES VARIABLE.
+let arrayRatesKeys = Object.keys(rates);
+
+let traditionalCurrenciesRates = traditionalCurrencies; // to assign this array to another one to keep the original unchanged.
+
+// ADDING PROPERTY RATES WITH THE VALUE TO THE OBJECTS IN THE TRADITIONALCURRENCIES ARRAY 
+traditionalCurrenciesRates.forEach( currency => {
+  if (arrayRatesKeys.includes(currency.currencyCode)){
+    let rate = rates[currency.currencyCode];
+    currency.rate = Number(rate); //value to be expressed in number type.
+  }
+});
+
+
+// SORTING THE ARRAY OF TRADITIONAL CURRENCIES RATES WITH .sort() methods 
+traditionalCurrenciesRates.sort(function (a, b) {
+  return b.rate - a.rate;
+});
+
+
+traditionalCurrenciesRates.splice(0, 19);
 
 export default function Home() {
   return (
     <Container>
-      <h1 className="main-heading">World Coins Rate</h1>
-      <h2 className="subheading">What is Lorem Ipsum?</h2>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
-        dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-         It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-         It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
-         with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-      <h2 className="subheading">Why do we use it?</h2>
-      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-        e point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
-        content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum
-         as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions
-          have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-
-
-
-      <p></p>
+      <h1 className="main-heading">supportedCurrencies {supportedCurrencies.length}</h1>
+      <h1 className="main-heading">traditionalCurrencies {traditionalCurrencies.length}</h1>
+      <h1 className="main-heading">cryptoCurrencies {cryptoCurrencies.length}</h1>
+      <h1 className="main-heading">latestRates {Object.keys(latestRates.rates).length}</h1>
+      <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
     </Container>
   );
   
