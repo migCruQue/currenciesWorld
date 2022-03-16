b2587b2e2563fc28aeeebf0ce4eeaeae656dda12
Miguel Cruz Quesada <miguel.cruz.quesada@gmail.com>
2022-03-16 17:34:48 +0000

    Input A and Input B working with fake API data

--- src/config/select8currencies.js
+++ src/config/select8currencies.js
@@ -9,3 +9,15 @@ export const selectCurrencies = [
     {"currencyCode":"AUD","currencyName":"Australian Dollar"},
     {"currencyCode":"ZAR","currencyName":"Rand"}
 ];
+
+export const dollarRatio = {
+    "USD": "1",
+    "EUR":"0.896909",
+    "JPY":"115.25504407",
+    "GBP":"0.74599",
+    "CHF":"0.931262",
+    "CAD":"1.27678",
+    "AUD":"1.430964",
+    "ZAR":"15.59333",
+};
+    
--- src/containers/Converter/converter.js
+++ src/containers/Converter/converter.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
 import SelectCurrency from './SelectCurrency/SelectCurrency';
 
+const currencyADollarRatio = 0.77;
+const currencyBDollarRatio = 0.83;
+
+
+function toCurrencyB(currencyAAmount){
+   return (currencyAAmount / currencyADollarRatio) * currencyBDollarRatio; 
+}
+
+function toCurrencyA(currencyBAmount){
+    return (currencyBAmount / currencyBDollarRatio) * currencyADollarRatio; 
+ }
+
+function tryConvert(currencyAmount, convert){
+    const input = parseFloat(currencyAmount);
+    if(Number.isNaN(input)){
+        return "";
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000; // rounds the output to the third decimal place.
+    return rounded.toString();
+
+}
+
 class Converter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleInputAChange = this.handleInputAChange.bind(this);
+    this.handleInputBChange = this.handleInputBChange.bind(this);
+    this.state = {amount: "", currentCurrency: "A"};
+  }
+
+  handleInputAChange(amount){
+    this.setState({currentCurrency: "A", amount});
+  }
+
+  handleInputBChange(amount){
+    this.setState({currentCurrency: "B", amount});
+  }
+
   render() {
+    const currentCurrency = this.state.currentCurrency;
+    const amount = this.state.amount;
+    const inputA = currentCurrency === "B" ? tryConvert(amount, toCurrencyB) : amount;
+    const inputB = currentCurrency === "A" ? tryConvert(amount, toCurrencyA) : amount;
     return (
       <div>
           <h1>Converter</h1>
-          <SelectCurrency />
+          <SelectCurrency position="A" amount={inputA} onInputChange={this.handleInputAChange} />
+          <SelectCurrency position="B" amount={inputB} onInputChange={this.handleInputBChange}/>
       </div>
     );
   }
